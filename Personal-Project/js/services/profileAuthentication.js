47ab820dbc0bad1b2d1f4d5687e0f115ef779cf0
RosenKrumov <rosenkrumov95@gmail.com>
2015-05-20 19:40:27 +0300

    added requests to the DB that get all friend invitations, accept invitations and reject invitations

--- Personal-Project/js/services/profileAuthentication.js
+++ Personal-Project/js/services/profileAuthentication.js
@@ -19,23 +19,53 @@ SocialNetwork.factory('profileAuthentication', function ($http, baseServiceUrl) 
             }).error(error);
     };
 
-    service.GetUserProfile = function (success, error) {
-        $http.get(baseServiceUrl + '/me', {headers: this.GetHeaders()})
+    service.GetMyProfile = function (success, error) {
+        $http.get(baseServiceUrl + '/me', {headers: this.GetHeaders() })
+            .success(function (data, status, headers, config) {
+                success(data)
+            }).error(error);
+    };
+
+    service.GetUserProfile = function (username, success, error) {
+        $http.get(baseServiceUrl + '/users/' + username , {headers: this.GetHeaders() })
             .success(function (data, status, headers, config) {
                 success(data)
             }).error(error);
     };
 
     service.EditUserProfile = function (editUserData, success, error) {
-        $http.put(baseServiceUrl + '/me', editUserData, {headers: this.GetHeaders()})
+        $http.put(baseServiceUrl + '/me', editUserData, { headers: this.GetHeaders() })
             .success(function (data, status, headers, config) {
                 success(data)
             }).error(error);
     };
 
-    service.ChangePassword = function (passwordData, success, error) {
-        $http.put(baseServiceUrl + '/me/ChangePassword', passwordData, {headers: this.GetHeaders()})
+    service.GetFriendRequests = function(headers, success, error) {
+        $http.get(baseServiceUrl + '/me/requests', { headers: headers })
             .success(function (data, status, headers, config) {
+                success(data);
+            }).error(error);
+    };
+
+    service.ChangePassword = function (passwordData, success, error) {
+        $http.put(baseServiceUrl + '/me/ChangePassword', passwordData, { headers: this.GetHeaders() })
+            .success(function (data, status, headers, config) {
+                success()
+            }).error(error);
+    };
+
+    service.ApproveRequest = function(id, success, error) {
+        var url = baseServiceUrl + '/me/requests/' + id + '?status=approved';
+        $http.put(url, null, { headers: this.GetHeaders() })
+            .success(function () {
+                success()
+            }).error(error);
+    };
+
+    service.RejectRequest = function(id, success, error) {
+        var url = baseServiceUrl + '/me/requests/' + id + '?status=rejected';
+        $http.put(url, null, { headers: this.GetHeaders() })
+            .success(function () {
                 success()
             }).error(error);
     };
