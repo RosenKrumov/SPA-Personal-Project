8e807eed7c1d1ef717f7e3e417ef8c4417f08613
RosenKrumov <rosenkrumov95@gmail.com>
2015-05-21 22:37:51 +0300

    implemented delete/edit/like/unlike post requests to the DB, implemented add/delete/edit/like/unlike comment requests to the DB

--- Personal-Project/js/services/postService.js
+++ Personal-Project/js/services/postService.js
@@ -16,7 +16,7 @@ SocialNetwork.factory('postService',
         };
 
         service.getUserPosts = function(id, headers, success, error) {
-            $http.get(baseServiceUrl + '/users/' + id + '/wall?PageSize=10', { headers: headers})
+            $http.get(baseServiceUrl + '/users/' + id + '/wall?PageSize=5', { headers: headers})
                 .success(function (data, status, headers, config) {
                     success(data);
                 }).error(error);
@@ -24,6 +24,69 @@ SocialNetwork.factory('postService',
 
         service.getNewsFeed = function(headers, success, error) {
             $http.get(baseServiceUrl + '/me/feed?PageSize=10', { headers: headers})
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.deletePost = function(id, headers, success, error) {
+            $http.delete(serviceUrl + id, { headers: headers })
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.editPost = function(id, data, headers, success, error) {
+            $http.put(serviceUrl + id, data, { headers: headers })
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.likePost = function(id, headers, success, error) {
+            $http.post(serviceUrl + id + '/likes', null, { headers: headers })
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.unlikePost = function(id, headers, success, error) {
+            $http.delete(serviceUrl + id + '/likes', { headers: headers })
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.addCommentToPost = function(id, data, headers, success, error) {
+            $http.post(serviceUrl + id + '/comments', data, { headers: headers })
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.deleteCommentOnPost = function(commentId, postId, headers, success, error) {
+            $http.delete(serviceUrl + postId + '/comments/' + commentId, { headers: headers })
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.editCommentOnPost = function(commentId, postId, data, headers, success, error) {
+            $http.put(serviceUrl + postId + '/comments/' + commentId, data, { headers: headers })
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.likeCommentOnPost = function(commentId, postId, headers, success, error) {
+            $http.post(serviceUrl + postId + '/comments/' + commentId + '/likes', null, { headers: headers })
+                .success(function (data, status, headers, config) {
+                    success(data);
+                }).error(error);
+        };
+
+        service.unlikeCommentOnPost = function(commentId, postId, headers, success, error) {
+            $http.delete(serviceUrl + postId + '/comments/' + commentId + '/likes', { headers: headers })
                 .success(function (data, status, headers, config) {
                     success(data);
                 }).error(error);
