1b5537b6a49b8760e859d268991ed5c8ee131cd1
RosenKrumov <rosenkrumov95@gmail.com>
2015-05-21 22:35:26 +0300

    added collapsing on clicking on the comments button, fixed userPosts function to be not self-executing, implemented adding comment to post functionality, implemented editing post functionality, implemented deleting post functionality, implemented liking/unliking post functionality, implemented deleting comment functionality, implemented editing comment functionality, implemented comment liking/unliking functionality

--- Personal-Project/js/controllers/postController.js
+++ Personal-Project/js/controllers/postController.js
@@ -3,47 +3,152 @@
 SocialNetwork.controller('postController',
     function($scope, $location, $route, $routeParams, postService, profileAuthentication, notifyService){
 
-        var getUserPosts = function (postId) {
+        $scope.username = sessionStorage['username'];
+        $scope.commentsClicked = {};
+
+        $scope.clickComments = function(id){
+            $scope.commentsClicked[id] = !$scope.commentsClicked[id];
+        };
+
+        $scope.checkCommentsClicked = function(id) {
+            return $scope.commentsClicked[id];
+        };
+
+        var getUserPosts = function () {
             if($routeParams.username) {
-                if (!postId) {
-                    var id = $routeParams.username;
-                    postService.getUserPosts(id, profileAuthentication.GetHeaders(),
-                        function (resp) {
-                            console.log(resp);
-                            $scope.userPosts = resp;
-                        });
-                } else {
-                    id = $routeParams.username;
-                    postService.getUserPosts(id, postId, profileAuthentication.GetHeaders(),
-                        function (resp) {
-                            console.log(resp);
-                            $scope.currentPost = resp;
-                        })
-                }
+                var id = $routeParams.username;
+                postService.getUserPosts(id, profileAuthentication.GetHeaders(),
+                    function (resp) {
+                        console.log(resp);
+                        $scope.userPosts = resp;
+                    });
             }
-        }();
+        };
+
+        getUserPosts();
 
         $scope.getNewsFeed = function() {
             postService.getNewsFeed(profileAuthentication.GetHeaders(),
                 function(data) {
                     $scope.news = data;
-                    console.log(data);
                 }, function(error){
                     console.log(error);
                 }
             )
-        }();
+        };
+
+        $scope.getNewsFeed();
 
         $scope.addPost = function () {
-            $scope.postData.Username = $routeParams.username;
+            $scope.postData.username = $routeParams.username;
             postService.addPost($scope.postData, profileAuthentication.GetHeaders(),
                 function() {
                     notifyService.showInfo("Successful Ad Publish!");
-                    $route.reload();
+                    $scope.userPosts = {};
+                    getUserPosts();
                 },
                 function (serverError) {
                     notifyService.showError("Unsuccessful Ad Publish!", serverError)
                 })
+        };
+
+        $scope.addCommentToPost = function(post) {
+            postService.addCommentToPost(post.id, $scope.commentData, profileAuthentication.GetHeaders(),
+                function(data) {
+                    post.comments.unshift(data);
+                    $scope.commentData = {};
+                },
+                function(serverError){
+                    console.log(serverError);
+                })
+        };
+
+        $scope.editPost = function (post) {
+            postService.editPost(post.id, post, profileAuthentication.GetHeaders(),
+                function() {
+                    $route.reload();
+                },
+                function (serverError) {
+                    console.log(serverError);
+                })
+        };
+
+        $scope.deletePost = function (post) {
+            postService.deletePost(post.id, profileAuthentication.GetHeaders(),
+                function() {
+                    $scope.userPosts = {};
+                    getUserPosts();
+                },
+                function (serverError) {
+                    console.log(serverError);
+                })
+        };
+
+        $scope.likePost = function(post) {
+            postService.likePost(post.id, profileAuthentication.GetHeaders(),
+                function() {
+                    post.liked = true;
+                    post.likesCount++;
+                },
+                function(serverError) {
+                    console.log(serverError);
+                }
+            )
+        };
+
+        $scope.unlikePost = function(post) {
+            postService.unlikePost(post.id, profileAuthentication.GetHeaders(),
+                function() {
+                    post.liked = false;
+                    post.likesCount--;
+                },
+                function(serverError) {
+                    console.log(serverError);
+                }
+            )
+        };
+
+        $scope.deleteCommentOnPost = function(post, comment) {
+            postService.deleteCommentOnPost(comment.id, post.id, profileAuthentication.GetHeaders(),
+                function() {
+                    var commentIndex = post.comments.indexOf(comment);
+                    post.comments.splice(commentIndex, 1);
+                }, function(error) {
+                    console.log(error);
+                }
+            )
+        };
+
+        $scope.editCommentOnPost = function(post, comment) {
+            postService.editCommentOnPost(comment.id, post.id, comment, profileAuthentication.GetHeaders(),
+                function() {
+                    $route.reload();
+                }, function(error) {
+                    console.log(error);
+                }
+            )
+        };
+
+        $scope.likeCommentOnPost = function(postId, comment) {
+            postService.likeCommentOnPost(comment.id, postId, profileAuthentication.GetHeaders(),
+                function() {
+                    comment.liked = true;
+                    comment.likesCount++;
+                }, function(error) {
+                    console.log(error);
+                }
+            )
+        };
+
+        $scope.unlikeCommentOnPost = function(postId, comment) {
+            postService.unlikeCommentOnPost(comment.id, postId, profileAuthentication.GetHeaders(),
+                function() {
+                    comment.liked = false;
+                    comment.likesCount--;
+                }, function(error) {
+                    console.log(error);
+                }
+            )
         };
     }
 );
