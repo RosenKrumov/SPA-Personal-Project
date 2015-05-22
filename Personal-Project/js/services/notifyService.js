273de66721028b614a2da41ce0961622660f7ead
RosenKrumov <rosenkrumov95@gmail.com>
2015-05-22 13:36:44 +0300

    changed notifying from info to success

--- Personal-Project/js/services/notifyService.js
+++ Personal-Project/js/services/notifyService.js
@@ -6,7 +6,7 @@ SocialNetwork.factory('notifyService',
             showInfo: function(msg) {
                 noty({
                     text: msg,
-                    type: 'info',
+                    type: 'success',
                     layout: 'topCenter',
                     timeout: 1000}
                 );
